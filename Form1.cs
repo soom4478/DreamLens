8bd14caf17d0bf9435ade2a1e341ca1e84c2e98a
soom <s2305@e-mirim.hs.kr>
2025-08-26 15:56:54 +0900

    사용자 입력 구현 #1

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,15 @@ namespace DreamLens
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
